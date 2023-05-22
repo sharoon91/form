6cf0b7f5888396cd8babdc481210ca2f0e13296d
Sharoon Kumar <134275580+sharoon91@users.noreply.github.com>
2023-05-22 16:06:44 +0500

    name.js

---
+++ name.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // Your code here
+  var firstNameInput = document.querySelector('#text-00000012');
+
+  var firstNameLabel = document.querySelector('#text-0000001e');
+
+  firstNameInput.addEventListener('input', function() {
+    // Update the label text with the entered value
+    firstNameLabel.value =  firstNameInput.value;
+  });
+});
